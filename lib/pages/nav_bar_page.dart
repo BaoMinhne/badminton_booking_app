6d1d26ae08127c03a0fbb98a4174ee50b2574a5f
BaoMinhne <130214731+minhb2105709@users.noreply.github.com>
2025-11-24 08:32:43 +0700

    fix error

--- lib/pages/nav_bar_page.dart
+++ lib/pages/nav_bar_page.dart
@@ -23,6 +23,10 @@ class _NavBarPageState extends State<NavBarPage> {
   @override
   void initState() {
     super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (!mounted) return;
+      context.read<UserManager>().loadMe(); // nạp cache lần đầu
+    });
 
     _pages = [
       HomePage(),
